892e9959b3e2c02de8f8403fdcaa57f80bb04554
vib <vibrunazo@gmail.com>
2025-06-14 15:39:48 -0300

    die on hit by enemy ai

---
+++ Source/DerpQuest/Public/AI/DerpAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "DerpAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DERPQUEST_API ADerpAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
