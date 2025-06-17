92f8935567114644a4b4abec9fc17643b2eeca0a
vib <vibrunazo@gmail.com>
2025-06-16 23:48:05 -0300

    timer runs out

--- Source/DerpQuest/Private/Game/DerpGameMode.cpp
+++ Source/DerpQuest/Private/Game/DerpGameMode.cpp
@@ -3,3 +3,31 @@
 
 #include "Game/DerpGameMode.h"
 
+ADerpGameMode::ADerpGameMode()
+{
+	TimeRemaining = TotalTime;
+}
+
+void ADerpGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	// Update every second
+	GetWorldTimerManager().SetTimer(TimerHandle, this, &ADerpGameMode::UpdateTimer, 1.0f, true);
+}
+
+void ADerpGameMode::UpdateTimer()
+{
+	if (TimeRemaining > 0)
+	{
+		TimeRemaining--;
+		OnTimerUpdate(TimeRemaining);
+	}
+	else
+	{
+		// Timer reached zero, restart level
+		GetWorldTimerManager().ClearTimer(TimerHandle);
+		UE_LOG(LogTemp, Warning, TEXT("Time's up!"));
+		// Uncomment to restart level when time runs out
+		// UGameplayStatics::OpenLevel(GetWorld(), FName(*GetWorld()->GetName()));
+	}
+}
--- Source/DerpQuest/Public/Game/DerpGameMode.h
+++ Source/DerpQuest/Public/Game/DerpGameMode.h
@@ -13,5 +13,28 @@ UCLASS()
 class DERPQUEST_API ADerpGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+public:
+	ADerpGameMode();
+
+	// Called when the timer updates
+	UFUNCTION(BlueprintImplementableEvent, Category = "Game")
+	void OnTimerUpdate(int32 NewTime);
 	
+	// Current time remaining
+	UPROPERTY(BlueprintReadOnly, Category = "Game")
+	int32 TimeRemaining;
+	
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	// Total time in seconds
+	UPROPERTY(EditAnywhere, Category = "Game")
+	int32 TotalTime = 60; // Default to 60 seconds
+
+
+	FTimerHandle TimerHandle;
+
+	// Update the timer
+	void UpdateTimer();
 };
